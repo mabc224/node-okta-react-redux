06c241eb6b7a73264a979882732a66fae0603548
mabc224 <mabc224@gmail.com>
2018-02-11 02:27:39 +0500

    fix redirect uri react

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,6 @@
 export default {
     url: 'https://dev-702756.oktapreview.com',
     issuer: 'https://dev-702756.oktapreview.com/oauth2/default',
-    redirect_uri: window.location.origin + '/implicit/callback',
+    redirect_uri: window.location.origin + '/node-okta-react-redux/implicit/callback',
     client_id: '0oadx5t9wnu6QMujD0h7'
 }
