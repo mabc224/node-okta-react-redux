ddc1007e611cf836089ee3a61ae22e6a887f5d7b
mabc224 <mabc224@gmail.com>
2018-02-11 03:29:36 +0500

    fix callback path in APP.js and proxy in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
                     <div className="container">
                         <main>
                             <Route path="/node-okta-react-redux" exact={true} component={HomePage} />
-                            <Route path="/implicit/callback" component={ImplicitCallback} />
+                            <Route path="/node-okta-react-redux/implicit/callback" component={ImplicitCallback} />
                             <Route path="/node-okta-react-redux/login" render={() => <LoginPage baseUrl={config.url} />} />
                             <Route path="/node-okta-react-redux/register" component={RegistrationForm} />
                             <SecureRoute path="/node-okta-react-redux/profile" component={ProfilePage} />
